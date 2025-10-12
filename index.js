d2e6c068c61ef70925a276be51dc1ec284ab64fa
pective <pective@proton.me>
2025-10-12 20:14:57 +0200

    Made search skeleton, works in console only for now

---
+++ index.js
@@ -0,0 +1,31 @@
+async function processJson(object) {
+	const obj = await object.json();
+
+	return {
+		temp: obj.currentConditions.temp,
+		condition: obj.currentConditions.conditions,
+		desc: obj.description,
+	};
+}
+
+async function getWeather(location) {
+	location.toLowerCase();
+
+	try {
+		const rawResponse = await fetch(
+			`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?unitGroup=metric&key=TL4R53JS4KYP4FFWVK6FZNGKS`
+		);
+		const response = await processJson(rawResponse);
+		console.log(response);
+		return response;
+	} catch (error) {
+		console.error(error);
+	}
+}
+
+const submit = document.querySelector("#searchBtn");
+
+submit.addEventListener("click", () => {
+	const location = document.querySelector("#locationSearch");
+	getWeather(location.value);
+});
